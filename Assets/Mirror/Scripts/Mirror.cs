ee59109243dc5de9e44e1894e24b37c287c248b9
yasuaki.ohama <yasuaki.ohama@gmail.com>
2015-07-05 03:54:58 +0900

    tern

--- Assets/Mirror/Scripts/Mirror.cs
+++ Assets/Mirror/Scripts/Mirror.cs
@@ -6,11 +6,29 @@ public class Mirror : MonoBehaviour
     private float MAP_RADIUS  = 10.0f;
     public Transform target;
 
-    void Update () {
+    void Start()
+    {
+        InvokeRepeating ("Tern", 1, 1);
+    }
+
+    void Tern()
+    {
+        target.transform.localScale = new Vector3 (target.transform.localScale.x * -1, target.transform.localScale.y, target.transform.localScale.z);
+    }
+
+    void Update ()
+    {
         Vector2 offset;
         offset.x = OffsetRange (-target.position.x / MAP_RADIUS);
         offset.y = OffsetRange (-target.position.y / MAP_RADIUS);
-        this.GetComponent<MeshRenderer> ().material.SetTextureOffset ("_MainTex", offset);
+        if (target.transform.localScale.x < 0) {
+            offset.x = -offset.x;
+            this.GetComponent<MeshRenderer> ().material.SetTextureOffset ("_MainTex", offset);
+            this.GetComponent<MeshRenderer> ().material.SetTextureScale ("_MainTex", new Vector2 (-1, 1));
+        } else {
+            this.GetComponent<MeshRenderer> ().material.SetTextureOffset ("_MainTex", offset);
+            this.GetComponent<MeshRenderer> ().material.SetTextureScale ("_MainTex", new Vector2 (1, 1));
+        }
     }
 
     float OffsetRange (float value)
--- Assets/Mirror/Shaders/MaskedLightTexture.shader
+++ Assets/Mirror/Shaders/MaskedLightTexture.shader
@@ -1,20 +1,29 @@
-﻿Shader "Custom/Mirror" {
+﻿Shader "MaskedLightTexture" {
     Properties {
+        _Color ("Main Color", Color) = (1,1,1,0)
+        _SpecColor ("Spec Color", Color) = (1,1,1,1)
+        _Emission ("Emmisive Color", Color) = (0,0,0,0)
+        _Shininess ("Shininess", Range (0.01, 1)) = 0.7
         _MainTex ("Base (RGB)", 2D) = "white" {}
         _Mask ("Culling Mask", 2D) = "white" {}
         _Cutoff ("Alpha cutoff", Range (0,1)) = 0.1
     }
     SubShader {
         Tags {"Queue"="Transparent"}
-        Lighting On
         ZWrite Off
         Blend SrcAlpha OneMinusSrcAlpha
         AlphaTest GEqual [_Cutoff]
-
+ 
         Pass {
             Material {
-                Diffuse (1, 1, 1, 1)
+                Diffuse [_Color]
+                Ambient [_Color]
+                Shininess [_Shininess]
+                Specular [_SpecColor]
+                Emission [_Emission]
             }
+            Lighting On
+            SeparateSpecular On
             SetTexture [_Mask] {combine texture}
             SetTexture [_MainTex] {combine texture * primary DOUBLE,texture * previous}
         }
